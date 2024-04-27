ab36e0b11ed4ee23685646ff76af3f6a1cabb046
anogray <bbncr97@gmail.com>
2024-04-28 01:29:21 +0530

    adding try catch main ts

--- src/main.ts
+++ src/main.ts
@@ -8,24 +8,29 @@ import { ValidationPipe } from '@nestjs/common';
 import cors from 'cors'
 
 async function bootstrap() {
-  dotenv.config(); 
-  const app = await NestFactory.create(AppModule,{cors:{origin:'*'}});
-  // app.enableCors({
+  try{
 
-  //   origin:"*"
-    
-  //   });
-    
-  // app.use(cors())
-  // app.use(cors({
-  //   origin: process.env.FRONTEND_ORIGIN || 'http://localhost:19002',
-  //   credentials: true, // Allow cookies for authentication
-  // }));
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalFilters(new AllExceptionsFilter());
-  app.useGlobalInterceptors(new ResponseInterceptor());
-
-
-  await app.listen(3000);
+    dotenv.config(); 
+    const app = await NestFactory.create(AppModule,{cors:{origin:'*'}});
+    // app.enableCors({
+  
+    //   origin:"*"
+      
+    //   });
+      
+    // app.use(cors())
+    // app.use(cors({
+    //   origin: process.env.FRONTEND_ORIGIN || 'http://localhost:19002',
+    //   credentials: true, // Allow cookies for authentication
+    // }));
+    app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalFilters(new AllExceptionsFilter());
+    app.useGlobalInterceptors(new ResponseInterceptor());
+  
+  
+    await app.listen(3000);
+  }catch(err){
+    console.log("errorMainFxn",err,err?.message)
+  }
 }
 bootstrap();
